98a61d32440367b2c7183d64912b271bd3bedf5a
krishnasaroj279 <krishnasaroj279@gmail.com>
2023-01-15 20:04:29 +0530

    hiding secret key

--- MyVid/settings.py
+++ MyVid/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&&7h4i7i=yw8!@we3mwi9mku4py1*p780u%nl)p&_+ccmgw=n'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
